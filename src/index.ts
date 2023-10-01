41e8b9d1ffcfd590288020411bc523e6118e6fa3
Matt Kenefick <keneficksays@gmail.com>
2023-10-01 09:22:48 -0400

    Update package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-export { default as Core } from '@chalkysticks/sdk-core';
-export { Collection } from '@chalkysticks/sdk-core';
-
+export * as Core from '@chalkysticks/sdk-core';
 export * as Model from './Model';
